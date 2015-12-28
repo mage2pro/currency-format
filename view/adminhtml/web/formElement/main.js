549f807e4dd4a85746295d9cee8f8b09d4bc7b04
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-12-28 17:47:56 +0300

    no message

--- view/adminhtml/web/formElement/main.js
+++ view/adminhtml/web/formElement/main.js
@@ -5,7 +5,20 @@ define(['jquery', 'domReady!'], function($) {return (
 	 * @param {String} config.id
 	 */
 	function(config) {
-		/** @type {jQuery}HTMLFieldSetElement */
+		/** @type {jQuery} HTMLFieldSetElement */
 		var $element = $(document.getElementById(config.id));
+		var updateDecimalSeparator = function(checkbox) {
+			$(checkbox).each(function() {
+				/** @type {jQuery} HTMLInputElement */
+				var $c = $(this);
+				$('.df-field.df-name-decimalSeparator', $c.closest('fieldset.df-currency-format'))
+					.toggle($c.is(':checked'))
+				;
+			});
+		};
+		/** @type {jQuery} HTMLInputElement[] */
+		var $showDecimals = $('input.df-name-showDecimals', $element);
+		$showDecimals.change(function() {updateDecimalSeparator(this);});
+		updateDecimalSeparator($showDecimals);
 	}
 );});
