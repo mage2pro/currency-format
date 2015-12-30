e243931b70e5c4d6c83b20fe3962ee081f3cf5d5
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-12-30 18:30:27 +0300

    no message

--- view/adminhtml/web/formElement/main.js
+++ view/adminhtml/web/formElement/main.js
@@ -23,6 +23,7 @@ define(['jquery', 'domReady!'], function($) {return (
 		/** @type {jQuery} HTMLFieldSetElement */
 		var $element = $(document.getElementById(config.id));
 		$element.hasClass('df-name-template')
+			// http://code.dmitry-fedyuk.com/m2/all/blob/b9f6c2f1c33bfdfd033f8c20d63afa4c54167d99/Framework/view/adminhtml/web/formElement/array/main.js#L115
 			? $(window).bind('df.config.array.add', function(event, $item) {prepare($item);})
 			: prepare($element)
 		;
