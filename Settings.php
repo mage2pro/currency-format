6b8aa61484e25cd6316e28ac9339060194a0e304
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-06-02 21:46:27 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -1,13 +1,13 @@
 <?php
 namespace Dfe\CurrencyFormat;
-use Magento\Framework\App\ScopeInterface;
+use Magento\Framework\App\ScopeInterface as S;
 class Settings extends \Df\Core\Settings {
 	/**
 	 * 2015-12-30
 	 * Форматирование отображения денежных денежных величин:
 	 * «Mage2.PRO» → «Currency» → «Format».
 	 * @param string|null $currencyCode [optional]
-	 * @param null|string|int|ScopeInterface $scope [optional]
+	 * @param null|string|int|S $scope [optional]
 	 * @return \Df\Config\A|O|null
 	 */
 	public function get($currencyCode = null, $scope = null) {
@@ -26,7 +26,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2015-12-26
 	 * «Mage2.PRO» → «Currency» → «Format» → «Enable?»
-	 * @param null|string|int|ScopeInterface $scope [optional]
+	 * @param null|string|int|S $scope [optional]
 	 * @return bool
 	 */
 	public function enable($scope = null) {return $this->b(__FUNCTION__, $scope);}
