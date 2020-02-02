8518b66127c429959ad21353bf72855b19712ade
Dmitry Fedyuk <dfediuk@gmail.com>
2020-02-02 09:21:20 +0600

    no message

--- view/adminhtml/web/formElement/main.js
+++ view/adminhtml/web/formElement/main.js
@@ -23,7 +23,7 @@ define(['jquery', 'domReady!'], function($) {return (
 		/** @type {jQuery} HTMLFieldSetElement */
 		var $element = $(document.getElementById(config.id));
 		$element.hasClass('df-name-template')
-			// https://github.com/mage2pro/core/tree/b9f6c2f1c33bfdfd033f8c20d63afa4c54167d99/Framework/view/adminhtml/web/formElement/array/main.js#L115
+			// https://github.com/mage2pro/core/blob/1.0.0/Framework/view/adminhtml/web/formElement/array/main.js#L116
 			? $(window).bind('df.config.array.add', function(event, $item) {prepare($item);})
 			: prepare($element)
 		;
