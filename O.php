dda1d1bb14a5eddc22f32a431788ce5575e25575
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-24 02:04:42 +0000

    refactoring

--- O.php
+++ O.php
@@ -83,6 +83,7 @@ final class O extends \Df\Config\ArrayItem {
 
 	/**
 	 * 2015-12-31
+	 * @used-by self::postProcess()
 	 * @used-by \Dfe\CurrencyFormat\Plugin\Framework\Locale\Format::aroundGetPriceFormat()
 	 */
 	function thousandsSeparator():string {
