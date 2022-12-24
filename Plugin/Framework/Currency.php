ebd282a212eed7a0a4573550b9e2f0120a39f5b6
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-24 02:57:12 +0000

    https://github.com/mage2pro/core/issues/174

--- Plugin/Framework/Currency.php
+++ Plugin/Framework/Currency.php
@@ -10,10 +10,9 @@ class Currency extends Sb {
 	/**
 	 * 2016-08-01
 	 * @see \Magento\Framework\Currency::toCurrency()
-	 * @param string $r
 	 * @return string
 	 */
-	function afterToCurrency(Sb $sb, $r) {
+	function afterToCurrency(Sb $sb, string $r) {
 		$s = Settings::s()->get($sb->_options['currency']); /** @var CFO $s */
 		return !$s ? $r : $s->postProcess($r, $sb->_options['locale']);
 	}
