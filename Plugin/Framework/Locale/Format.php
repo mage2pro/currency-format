0454128d4f0f866f25b340726d03bdaaed147a30
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-24 02:59:12 +0000

    https://github.com/mage2pro/core/issues/174

--- Plugin/Framework/Locale/Format.php
+++ Plugin/Framework/Locale/Format.php
@@ -27,7 +27,7 @@ class Format extends Sb {
 	 * @see \Dfe\CurrencyFormat\Plugin\Framework\Pricing\Render\Amount::beforeFormatCurrency()
 	 * 3) @see \Magento\Framework\Locale\Format::getPriceFormat()
 	 * https://github.com/magento/magento2/blob/2.0.0/lib/internal/Magento/Framework/Locale/Format.php#L89-L152
-	 * @param string $currencyCode
+	 * @param string|null $currencyCode
 	 * @return array(string => mixed)
 	 */
 	function aroundGetPriceFormat(Sb $sb, \Closure $f, string $localeCode = null, $currencyCode = null) {
