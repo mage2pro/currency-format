f161b043b79734db96a1cf1e44eb2d31038fb7f7
Dmitrii Fediuk <dfediuk@gmail.com>
2023-08-06 05:49:11 +0100

    refactoring

--- Plugin/Sales/Block/Adminhtml/Items/AbstractItems.php
+++ Plugin/Sales/Block/Adminhtml/Items/AbstractItems.php
@@ -4,13 +4,21 @@ use Dfe\CurrencyFormat\Settings;
 use Magento\Sales\Block\Adminhtml\Items\AbstractItems as Sb;
 class AbstractItems {
 	/**
-	 * 2016-08-03
-	 * https://mage2.pro/t/1929
+	 * 2016-08-03 https://mage2.pro/t/1929
 	 * @see \Magento\Sales\Block\Adminhtml\Items\AbstractItems::displayPrices()
 	 * https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Sales/Block/Adminhtml/Items/AbstractItems.php#L276-L288
+	 * @param float|null $basePrice
+	 * @param float|null $price
 	 */
 	function aroundDisplayPrices(
-		Sb $sb, \Closure $f, float $basePrice, float $price, bool $strong = false, string $sep = '<br />'
+		Sb $sb, \Closure $f
+		/**
+		 * 2023-08-05
+		 * Magento 2.4.7-beta1 can pass `null` as a money amount:
+		 * @see \Dfe\CurrencyFormat\Plugin\Directory\Model\Currency::aroundFormat()
+		 */
+		,$basePrice, $price
+		,bool $strong = false, string $sep = '<br />'
 	):string {
 		/** @var string $r */
 		Settings::ignorePrecision(true);
