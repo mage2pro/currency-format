2aa9aec1ed08b22897696cc5389db1873c66b294
Dmitrii Fediuk <dfediuk@gmail.com>
2023-08-06 05:46:55 +0100

    https://github.com/mage2pro/core/issues/325

--- Plugin/Framework/Pricing/PriceCurrencyInterface.php
+++ Plugin/Framework/Pricing/PriceCurrencyInterface.php
@@ -3,6 +3,7 @@ namespace Dfe\CurrencyFormat\Plugin\Framework\Pricing;
 use Dfe\CurrencyFormat\O as CFO;
 use Dfe\CurrencyFormat\Settings;
 use Magento\Directory\Model\Currency;
+use Magento\Framework\Pricing\PriceCurrencyInterface as IPriceCurrency;
 use Magento\Framework\Pricing\PriceCurrencyInterface as Sb;
 # 2015-12-26
 final class PriceCurrencyInterface {
@@ -18,19 +19,29 @@ final class PriceCurrencyInterface {
 	 * https://github.com/magento/magento2/blob/2.0.0/lib/internal/Magento/Framework/Pricing/PriceCurrencyInterface.php#L42-L58
 	 * 4) @see \Magento\Directory\Model\PriceCurrency::format()
 	 * https://github.com/magento/magento2/blob/2.0.0/app/code/Magento/Directory/Model/PriceCurrency.php#L70-L82
-	 * 2023-08-05
-	 * «Dfe\CurrencyFormat\Plugin\Framework\Pricing\PriceCurrencyInterface::beforeFormat():
-	 * Argument #2 ($a) must be of type float, null given»: https://github.com/mage2pro/currency-format/issues/13
 	 * @used-by \Magento\Framework\Pricing\Render\Amount::formatCurrency():
 	 *		return $this->priceCurrency->format($amount, $includeContainer, $precision);
 	 * https://github.com/magento/magento2/blob/2.4.7-beta1/lib/internal/Magento/Framework/Pricing/Render/Amount.php#L214-L228
-	 * @param float|null $a
-	 * @param int|null $precision [optional]
 	 * @param null|string|bool|int|\Magento\Framework\App\ScopeInterface $scope [optional]
 	 * @param \Magento\Framework\Model\AbstractModel|string|null $currency [optional]
 	 */
 	function beforeFormat(
-		Sb $sb, $a, bool $includeContainer = true, $precision = null, $scope = null, $currency = null
+		Sb $sb
+		/**
+		 * 2023-08-05
+		 * 1) «Dfe\CurrencyFormat\Plugin\Framework\Pricing\PriceCurrencyInterface::beforeFormat():
+		 * Argument #2 ($a) must be of type float, null given»: https://github.com/mage2pro/currency-format/issues/13
+		 * 2) Magento 2.4.7-beta1 can pass `null` as a money amount:
+		 * @see \Dfe\CurrencyFormat\Plugin\Directory\Model\Currency::aroundFormat()
+		 */
+		,$a
+		,bool $includeContainer = true
+		# 2023-08-06
+		# 1) "Declare optional argument values for intercepted methods": https://github.com/mage2pro/core/issues/325
+		# 2) "Magento does not pass the values of missed optional arguments of intercepted methods to plugins":
+		# https://mage2.pro/t/6378
+		,int $precision = IPriceCurrency::DEFAULT_PRECISION
+		,$scope = null, $currency = null
 	):array {
 		# 2015-12-31 https://github.com/magento/magento2/blob/2.0.0/app/code/Magento/Directory/Model/PriceCurrency.php#L80
 		$currencyModel = $sb->getCurrency($scope, $currency); /** @var Currency $currencyModel */
