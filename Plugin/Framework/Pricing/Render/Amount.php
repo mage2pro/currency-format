70db39bb61291c6049e1ff35a55dd033961cf3e8
Dmitrii Fediuk <dfediuk@gmail.com>
2023-12-31 10:37:10 +0300

    https://github.com/mage2pro/core/issues/345

--- Plugin/Framework/Pricing/Render/Amount.php
+++ Plugin/Framework/Pricing/Render/Amount.php
@@ -10,7 +10,10 @@ use Magento\Framework\Pricing\Render\Amount as Sb;
 # 2023-08-06
 # "Prevent interceptors generation for the plugins extended from interceptable classes":
 # https://github.com/mage2pro/core/issues/327
-class Amount extends Sb	implements \Magento\Framework\ObjectManager\NoninterceptableInterface {
+# 2023-12-31
+# "Declare as `final` the final classes implemented `\Magento\Framework\ObjectManager\NoninterceptableInterface`"
+# https://github.com/mage2pro/core/issues/345
+final class Amount extends Sb	implements \Magento\Framework\ObjectManager\NoninterceptableInterface {
 	/**
 	 * 2016-01-01
 	 * An empty constructor allows us to skip the parent's one.
